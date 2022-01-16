701ca19356801245856d443f7963600a7a153360
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2022-01-16 20:27:05 +0530

    Update instaPhotoDownload.py

--- Python/instaPhotoDownload.py
+++ Python/instaPhotoDownload.py
@@ -12,7 +12,6 @@ def pic_download(instagram_username_profile_pic):
         os.chdir("Instagram Downloads")
         return access_bot.downlaod_profile(instagram_username_profile_pic, profile_pic_only=True)
 
-
 if __name__ == "__main__":
     instagram_username_profile_pic = input(
         "Enter the username of the account whose profile pic ypu want to download")

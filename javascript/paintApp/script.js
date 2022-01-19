b0c21b9fe5a1cb43f20a56b0a6b1e348b3b1c4b2
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2022-01-19 19:11:42 +0530

    Update script.js

--- javascript/paintApp/script.js
+++ javascript/paintApp/script.js
@@ -1,3 +1,4 @@
+
 const loadStorageButton = document.getElementById('load-storage');
 const saveStorageButton = document.getElementById('save-storage');
 const clearStorageButton = document.getElementById('clear-storage');
